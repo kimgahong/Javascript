5cc05256ab9301165c4428d28c5c120b101a1035
kimgahong <ghong1108@naver.com>
2023-01-04 18:54:35 +0900

    Commit Message

--- 김가홍/아카이브/test.js
+++
@@ -1,6 +0,0 @@
-// when using ES6 import / npm
-import ScrollTrigger from '@terwanerik/scrolltrigger'
-// Create a new ScrollTrigger instance with default options
-const trigger = new ScrollTrigger.default() // When not using npm, create a new instance with 'new ScrollTrigger.default()'
-// Add all html elements with attribute data-trigger
-trigger.add('[data-trigger]')
